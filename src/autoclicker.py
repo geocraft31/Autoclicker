52de38f56c62efecbabba6f0b62dd1ce5a1cf81d
Geo <geocraft31@gmail.com>
2023-06-27 18:59:18 +0200

    Added docstrings

--- src/autoclicker.py
+++ src/autoclicker.py
@@ -4,37 +4,83 @@ from pynput.mouse import Button, Controller
 
 
 class Autoclicker:
+    """
+    Autoclicker object.
+
+    ### Arguments
+
+    delay: float
+        Delay between each click
+    
+    mouseButton: pynupt.mouse.Button
+        The mouse button that the autoclicker targets
+
+    Attributes
+    ----
+    active: bool
+        The current state of the autoclicker
+
+    Methods
+    ----
+    start()
+        Starts the autoclicker
+    
+    stop()
+        Stops the autoclicker
+
+    run( time: float )
+        Runs the autoclicker for `time` seconds
+
+    """
     def __init__(self, delay: float, mouseButton: Button) -> None:
+
         self.delay = delay
-        self.mouse = Controller()
+        self._mouse = Controller()
         self.mouseButton = mouseButton
-        self.clickingThread = Thread(target=self._clicker, name="Clicker")
-        self.stopEvent = Event()
+        self._clickingThread = Thread(target=self._clicker, name="Clicker")
+        self._stopEvent = Event()
         self.active = False
 
         
     def start(self):
-        self.stopEvent.clear()
+        """Starts the autoclicker object
+        """
+        self._stopEvent.clear()
         self.active = True
-        if not self.clickingThread.is_alive():
-            self.clickingThread = Thread(target=self._clicker, name="Clicker")
-            self.clickingThread.start()
+        if not self._clickingThread.is_alive():
+            self._clickingThread = Thread(target=self._clicker, name="Clicker")
+            self._clickingThread.start()
     
     def stop(self):
-        if self.stopEvent.is_set():
+        """Stops the autoclicker object
+        """
+        if self._stopEvent.is_set():
             return
         self.active = False
-        self.stopEvent.set()
-        if self.clickingThread.is_alive():
-            self.clickingThread.join()
+        self._stopEvent.set()
+        if self._clickingThread.is_alive():
+            self._clickingThread.join()
 
 
     def run(self, timeSec: float):
+        """Starts the autoclicker for `timeSec` seconds
+
+        Args:
+            timeSec (float): The time the autoclicker is running
+        
+        Raises:
+            ValueError: if `timeSec` <= 0
+        """
+        if timeSec <= 0:
+            raise ValueError("Variable `timeSec` can't be 0 or below")
+
         self.start()
         time.sleep(timeSec)
         self.stop()
 
     def _clicker(self):
-        while not self.stopEvent.is_set():
-            self.mouse.click(self.mouseButton)
-            time.sleep(self.delay)
+        # private! Not for public use
+        
+        while not self._stopEvent.is_set():
+            self._mouse.click(self._mouseButton)
+            time.sleep(self._delay)
